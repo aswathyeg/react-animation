85b8b68d6dce61e777b6f518716c514d24fbc7c4
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-02-29 23:59:04 +0530

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 
-import './App.css'
+import ChallengePage from './components/pages/Challenge.jsx'
+import WelcomePage from './components/pages/Welcome.jsx'
+const router=createBrowserRouter([
+  {
+    path:'/',element:<WelcomePage/>,
 
+  },
+  {
+    path:'/challenges',element:<ChallengePage/>
+  }
+])
 function App() {
  
 
   return (
-    <>
-     
-    </>
+   <RouterProvider router={router}/>
   )
 }
 
