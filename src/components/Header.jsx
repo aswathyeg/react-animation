8e70a0b9076565ffee0faeac5fdf608425f4b8b4
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-03-23 22:21:01 +0530

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Your Challenges</h1>
+      <button>AddChallange</button>
+    </div>
+  );
+};
+
+export default Header;
